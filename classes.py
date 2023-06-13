e47c55c0c7546424db56d75af456d18a7c31c41d
Jan Kellner <jank.bewerbungen@hotmail.de>
2023-06-13 12:55:25 +0200

    changes for readability purposes

--- classes.py
+++ classes.py
@@ -15,13 +15,21 @@ class User():
         self.username = new_username
         self.alter = new_alter
         self.geschlecht = new_geschlecht
-        user_dict = {"Benutzername": self.username, "Alter": self.alter, "Geschlecht": self.geschlecht} #soll später in Tabelle "Benutzer" eingefügt werden        
+        user_dict = {
+            "Benutzername": self.username,
+            "Alter": self.alter,
+            "Geschlecht": self.geschlecht
+        } #soll später in Tabelle "Benutzer" eingefügt werden        
         return f'Benutzer: {self.username} erfolgreich erstellt!'
 
     def change_username(self, new_username):
         """Method to change a users name"""
         self.username = new_username
-        user_dict = {"Benutzername": self.username, "Alter": self.alter, "Geschlecht": self.geschlecht} #soll später in Tabelle "Benutzer" eingefügt werden
+        user_dict = {
+            "Benutzername": self.username,
+            "Alter": self.alter,
+            "Geschlecht": self.geschlecht
+        } #soll später in Tabelle "Benutzer" eingefügt werden
         return f'Benutzername erfolgreich zu: {self.username} geändert!'
     
 class Measurement():
@@ -30,7 +38,7 @@ class Measurement():
     def __init__(self, username, today, bodyweight, bodyheight):
         """Constructor for every measurement"""
         self.username = User.username
-        self.today = today = date.today()
+        self.today = date.today()
         self.bodyweight = bodyweight
         self.bodyheight = bodyheight
 
@@ -41,9 +49,16 @@ class Calculation():
     def __init__(self, username, today, bmi, calories):
         """Constructor for the class Calculation"""
         self.username = User.username
-        self.today = today = date.today()
+        self.today = date.today()
         self.bmi = bmi
         self.calories = calories
 
     def calculate_bmi(self, bodyweight, bodyheight):
         """Method to to calculate the bmi"""
+        self.bmi = bodyweight / (bodyheight ** 2)
+        bmi_dict = {
+            "Benutzername": self.username,
+            "Datum": self.today,
+            "BMI": self.bmi,
+            "Kalorien": self.calories
+        }
